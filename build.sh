9f7899fce7d09a8870ce270f2c93bc69ddbdd23b
Ezeagu Kosi <kosialex66@gmail.com>
2025-12-28 03:48:33 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python backend/main/manage.py collectstatic --no-input
 
-python manage.py migrate
+python backend/main/manage.py migrate
