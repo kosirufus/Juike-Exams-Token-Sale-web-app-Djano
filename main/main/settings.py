08e36e1134c327352e8196d00c9f6d2ef54f7301
Ezeagu Kosi <kosialex66@gmail.com>
2025-12-29 03:29:38 +0100

    Remove embedded juike-clean repo from tracking

--- main/main/settings.py
+++ main/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d$c4iqv_wsql_q6xy-5nw4ft)o9atov!&7a==c4d1q14e3z_5n"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
