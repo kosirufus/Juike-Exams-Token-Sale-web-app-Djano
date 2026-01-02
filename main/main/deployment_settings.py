b9934d5075599afebccbb6c99b6553d840431c8e
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-02 07:47:36 +0100

    updated allowed origins path

--- main/main/deployment_settings.py
+++ main/main/deployment_settings.py
@@ -29,9 +29,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-#CORS_ALLOWED_ORIGINS = [
-#    "http://localhost:3000",
-#]
+CORS_ALLOWED_ORIGINS = [
+    "https://juikeexams.onrender.com",
+]
 
 STORAGES = {
     "default": {
