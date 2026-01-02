d3c98f79d60594b0b358db41c319d2f4c3c4cc41
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-02 06:20:19 +0100

    superuser updated

--- main/build.sh
+++ main/build.sh
@@ -5,3 +5,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+
+if [$CREATE_SUPERUSER == "True"]; then
+    python manage.py createsuperuser --noinput
+fi
