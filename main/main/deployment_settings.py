c3cd41ac22270cc5b5459a205994c3ed906e9a10
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-02 04:50:58 +0100

    allowed hosts update

--- main/main/deployment_settings.py
+++ main/main/deployment_settings.py
@@ -3,7 +3,12 @@ from backend.main import dj_database_url
 from .settings import *  
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = (os.environ.get('RENDER_EXTERNAL_HOST'))
+ALLOWED_HOSTS = [
+    (os.environ.get('RENDER_EXTERNAL_HOST')),
+    'localhost',
+    '127.0.0.1',
+]
+
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOST')]
 
 DEBUG = False
