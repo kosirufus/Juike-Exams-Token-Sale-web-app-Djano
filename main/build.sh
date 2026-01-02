201871bdbfcf40401ebc7801ea09b159d573bbbd
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-02 06:56:17 +0100

    final superuser set

--- main/build.sh
+++ main/build.sh
@@ -2,37 +2,39 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py migrate
-python manage.py collectstatic --no-input
 
-python - << 'EOF'
-import os
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+
+#python - << 'EOF'
+#import os
 
 #Tells Django which settings to use
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.deployment_settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.deployment_settings")
 
 #Initialize Django
-import django
-django.setup()
+#import django
+#django.setup()
 
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 
-User = get_user_model()
+#User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+#username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+#email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+#password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if username and email and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
-else:
-    print("Superuser env vars not set")
-EOF
+#if username and email and password:
+    #if not User.objects.filter(username=username).exists():
+        #User.objects.create_superuser(
+            #username=username,
+            #email=email,
+            #password=password
+        #)
+        #print("Superuser created")
+    #else:
+        #print("Superuser already exists")
+#else:
+    #print("Superuser env vars not set")
+#EOF
